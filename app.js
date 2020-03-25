feeb5d632c286b66fb2cda46a432d3aedbe8f6bb
frontend-fxs <oriol@fxstreet.com>
2020-03-25 08:36:16 +0100

    update

--- app.js
+++ app.js
@@ -2,57 +2,76 @@ async function scrapPage() {
     let puppeteer = require('puppeteer');
     let psl = require('psl');
     let MongoClient = require('mongodb').MongoClient;
-    let extractHostname = require('extractHostname');
+    let extractHostname = require('./utils.js').extractHostname;
     let url = "mongodb://localhost:27017/";
     let client = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
     let webscrapper = client.db("webscrapper");
     let URL = '';
     let browser = await puppeteer.launch();
     let page = await browser.newPage();
-    
+    page.on('console', consoleObj => console.log(consoleObj.text()));
+    const res = await webscrapper.collection("urlsclasses").findOne({ visited: false });
+    URL = res.url;
+    let domain = psl.get(extractHostname(URL));
+
     try {
-        const res = await webscrapper.collection("urlsclasses").findOne({ visited: false });
-        URL = res.url;
-        let domain = psl.get(extractHostname(URL));
+
         await page.goto(URL, { waitUntil: 'domcontentloaded' });
-        await page.evaluate((domain) => {
+
+        let dataObj = await page.evaluate(() => {
+
             let anchors = document.querySelectorAll('a');
             let elements = document.querySelectorAll('*');
             let links = new Set();
             let classes = new Set();
+
             for (let i = 0; i < anchors.length; i++) {
                 let href = anchors[i].getAttribute("href");
-                if (/^\//.test(href)) {
-                    href = domain + href;
-                };
-                if (/\.fxstreet\./.test(href)) {
-                    links.add(href);
-                    let linkNotExist = webscrapper.collection('urlsclasses').find({ "url": href }).length < 1;
-                    if (linkNotExist) {
-                        webscrapper.collection('urlsclasses').insertOne({ url: href, visited: false });
-                    };
-                };
-            };
+                links.add(href);
+            }
+
             for (let i = 0; i < elements.length; i++) {
                 for (let j = 0; j < elements[i].classList.length; j++) {
                     let className = elements[i].classList.item(j);
                     classes.add(className);
-                    let classNotExist = webscrapper.collection('classes').find({ "className": className }).length < 1;
-                    if (classNotExist) {
-                        webscrapper.collection('classes').insertOne({ className: className });
-                    };
+                }
+            }
+            return {links:[...links],classes:[...classes]};
+        });
+        
+        for (let i = 0; i < dataObj.links.length;i++){
+            
+            href = dataObj.links[i];
+
+            if (/^\//.test(href)) {
+                href = domain + href;
+            };
+            if (/\.fxstreet\./.test(href)) {
+                let linkNotExist = webscrapper.collection('urlsclasses').find({ "url": href }).length < 1;
+                if (linkNotExist) {
+                    webscrapper.collection('urlsclasses').insertOne({ url: href, visited: false });
                 };
             };
-            webscrapper.collection('urlsclasses').insertOne({ url: URL, visited: true, classes: [...classes] });
-            scrapPage();
-        }, domain);
-    } catch (err) {
-        console.log('All links visited');
+        };
+        webscrapper.collection('urlsclasses').insertOne({ url: URL, visited: true, classes: [...dataObj.classes] });
+        scrapPage();
+        for (let i = 0; i < dataObj.classes.length;i++){
+            let className = dataObj.classes[i];
+            let classNotExist = webscrapper.collection('classes').find({ "className": className }).length < 1;
+            if (classNotExist) {
+                webscrapper.collection('classes').insertOne({ className: className });
+            };
+        }
+        
+    }
+    catch (err) {
         console.log(err);
-    } finally {
+    }
+    finally {
         client.close();
     };
     await browser.close();
+
 };
 (async () => {
     await scrapPage();
